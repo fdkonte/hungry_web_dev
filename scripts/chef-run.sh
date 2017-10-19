c11e9c1bfeb7b0f9abbc9863394d517d7ab09964
“Jack <jack.konte@gmail.com>
2017-10-19 09:30:56 -0700

    initial commit

---
+++ scripts/chef-run.sh
@@ -0,0 +1,2 @@
+cd /etc/chef/codedeploy/cookbooks/hungry_web_dev
+sudo chef-client -c /etc/chef/codedeploy/cookbooks/hungry_web_dev/client.rb -z -j /etc/chef/codedeploy/cookbooks/hungry_web_dev/dev.json
